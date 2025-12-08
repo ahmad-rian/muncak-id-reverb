35c78743c51bafa08483f6556f97dc398d27880c
AHMAD RIAN <alriansr@gmail.com>
2025-12-08 11:42:16 +0700

    update error

--- config/reverb.php
+++ config/reverb.php
@@ -5,11 +5,6 @@ return [
     |--------------------------------------------------------------------------
     | Default Reverb Server
     |--------------------------------------------------------------------------
-    |
-    | This option controls the default server used by Reverb to handle
-    | incoming messages as well as broadcasting message to all your
-    | connected clients. At this time only "reverb" is supported.
-    |
     */
 
     'default' => env('REVERB_SERVER', 'reverb'),
@@ -18,19 +13,19 @@ return [
     |--------------------------------------------------------------------------
     | Reverb Servers
     |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the Reverb servers that should be available
-    | for your application.
-    |
     */
 
     'servers' => [
-        [
-            'host' => env('REVERB_HOST', '0.0.0.0'),
-            'port' => env('REVERB_PORT', 8080),
-            'hostname' => env('REVERB_HOSTNAME', 'localhost'),
+
+        'reverb' => [
+            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
+            'port' => env('REVERB_SERVER_PORT', 8080),
+            'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert' => env('REVERB_TLS_CERT'),
+                    'local_pk' => env('REVERB_TLS_KEY'),
+                ],
             ],
             'scaling' => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
@@ -38,40 +33,45 @@ return [
             ],
             'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
+            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10000),
         ],
+
     ],
 
     /*
     |--------------------------------------------------------------------------
     | Reverb Applications
     |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the Reverb applications.
-    |
     */
 
     'apps' => [
-        [
-            'app_id' => env('REVERB_APP_ID', 'app-id'),
-            'app_key' => env('REVERB_APP_KEY'),
-            'app_secret' => env('REVERB_APP_SECRET'),
-            'options' => [
-                'host' => env('REVERB_HOST', '0.0.0.0'),
-                'port' => env('REVERB_PORT', 8080),
-                'scheme' => env('REVERB_SCHEME', 'http'),
-                'useTLS' => env('REVERB_SCHEME', 'http') === 'https',
-            ],
-            'allowed_origins' => ['*'],
-            'ping_interval' => env('REVERB_SERVER_PING_INTERVAL', 30),
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10000),
 
-            // ✅ OPTIMIZATION: Connection pooling and limits
-            'capacity' => [
-                'max_connections' => env('REVERB_MAX_CONNECTIONS', 10000),
-                'max_backend_events_per_sec' => env('REVERB_MAX_BACKEND_EVENTS_PER_SEC', 1000),
-                'max_client_messages_per_sec' => env('REVERB_MAX_CLIENT_MESSAGES_PER_SEC', 100),
-                'max_read_buffer' => env('REVERB_MAX_READ_BUFFER', 10240),
+        'provider' => 'config',
+
+        'apps' => [
+            [
+                'key' => env('REVERB_APP_KEY'),
+                'secret' => env('REVERB_APP_SECRET'),
+                'app_id' => env('REVERB_APP_ID'),
+                'allowed_origins' => ['*'],
+                'ping_interval' => env('REVERB_SERVER_PING_INTERVAL', 30),
+                'max_message_size' => env('REVERB_MAX_REQUEST_SIZE', 10000),
+                'options' => [
+                    'host' => env('REVERB_HOST'),
+                    'port' => env('REVERB_PORT', 443),
+                    'scheme' => env('REVERB_SCHEME', 'https'),
+                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                ],
+                // ✅ OPTIMIZATION: Connection limits
+                'capacity' => [
+                    'max_connections' => env('REVERB_MAX_CONNECTIONS', 10000),
+                    'max_backend_events_per_sec' => env('REVERB_MAX_BACKEND_EVENTS_PER_SEC', 1000),
+                    'max_client_messages_per_sec' => env('REVERB_MAX_CLIENT_MESSAGES_PER_SEC', 100),
+                    'max_read_buffer' => env('REVERB_MAX_READ_BUFFER', 10240),
+                ],
             ],
         ],
+
     ],
+
 ];
